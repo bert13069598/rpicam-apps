861ffe31248982862ce75ac67a13d97de675d673
bert13069598 <everglow83@naver.com>
2024-03-27 16:10:00 +0900

    saturation, value

--- post_processing_stages/inrange_stage.cpp
+++ post_processing_stages/inrange_stage.cpp
@@ -34,7 +34,7 @@ public:
 private:
 	Stream *stream_;
 	
-	int low_hue1, low_hue2, high_hue1, high_hue2, range_count;
+	int low_hue1, low_hue2, high_hue1, high_hue2, low_saturation, low_value;
 	Scalar color;
 };
 
@@ -51,6 +51,8 @@ void InrangeStage::Read(boost::property_tree::ptree const &params)
         int g = params.get<int>("color.g", 0);
         int r = params.get<int>("color.r", 255);
         color = Scalar(b, g, r);
+		low_saturation = params.get<int>("low_saturation", 50);
+		low_value = params.get<int>("low_value", 50);
 }
 
 void InrangeStage::Configure()
@@ -72,26 +74,19 @@ void InrangeStage::Configure()
 	high_hue1 = 0;
 	high_hue2 = 0;
 
-	range_count = 0;
 	if (low_hue < 10 ) {
-		range_count = 2;
-
 		high_hue1 = 180;
 		low_hue1 = low_hue + 180;
 		high_hue2 = high_hue;
 		low_hue2 = 0;
 	}
 	else if (high_hue > 170) {
-		range_count = 2;
-
 		high_hue1 = low_hue;
 		low_hue1 = 180;
 		high_hue2 = high_hue - 180;
 		low_hue2 = 0;
 	}
 	else {
-		range_count = 1;
-
 		low_hue1 = low_hue;
 		high_hue1 = high_hue;
 	}
@@ -114,20 +109,18 @@ bool InrangeStage::Process(CompletedRequestPtr &completed_request)
 	Mat imageMat(info.height, info.width, CV_8UC3, rgb_image.data());
 	cvtColor(imageMat, imageMat, COLOR_RGB2HSV);
 
-	Mat img_mask1, img_mask2;
-	inRange(imageMat, Scalar(low_hue1, 50, 50), Scalar(high_hue1, 255, 255), img_mask1);
-	if (range_count == 2) {
-		inRange(imageMat, Scalar(low_hue2, 50, 50), Scalar(high_hue2, 255, 255), img_mask2);
-		img_mask1 |= img_mask2;
-	}
+	Mat img_mask, img_mask1, img_mask2;
+	inRange(imageMat, Scalar(low_hue1, low_saturation, low_value), Scalar(high_hue1, 255, 255), img_mask1);
+	inRange(imageMat, Scalar(low_hue2, low_saturation, low_value), Scalar(high_hue2, 255, 255), img_mask2);
+	img_mask = img_mask1 | img_mask2;
 
-	erode(img_mask1, img_mask1, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
-	dilate(img_mask1, img_mask1, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
-	dilate(img_mask1, img_mask1, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
-	erode(img_mask1, img_mask1, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+	erode(img_mask, img_mask, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+	dilate(img_mask, img_mask, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+	dilate(img_mask, img_mask, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+	erode(img_mask, img_mask, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
 
 	Mat img_labels, stats, centroids;
-	int numOfLables = connectedComponentsWithStats(img_mask1, img_labels, stats, centroids, 8, CV_32S);
+	int numOfLables = connectedComponentsWithStats(img_mask, img_labels, stats, centroids, 8, CV_32S);
 
 	int max = -1, idx = 0;
 	for (int j = 1; j < numOfLables; j++) {
@@ -138,15 +131,17 @@ bool InrangeStage::Process(CompletedRequestPtr &completed_request)
 		}
 	}
 
-	int left = stats.at<int>(idx, CC_STAT_LEFT);
-	int top = stats.at<int>(idx, CC_STAT_TOP);
-	int width = stats.at<int>(idx, CC_STAT_WIDTH);
-	int height = stats.at<int>(idx, CC_STAT_HEIGHT);
+	if (max != -1){
+		int left = stats.at<int>(idx, CC_STAT_LEFT);
+		int top = stats.at<int>(idx, CC_STAT_TOP);
+		int width = stats.at<int>(idx, CC_STAT_WIDTH);
+		int height = stats.at<int>(idx, CC_STAT_HEIGHT);
 
-	rectangle(src, Point(left, top), Point(left + width, top + height), Scalar(255, 255, 255), 2);
+		rectangle(src, Point(left, top), Point(left + width, top + height), Scalar(255, 255, 255), 2);
+	}
 
-	resize(img_mask1, img_mask1, Size(info.width / 4, info.height / 4), 0, 0, INTER_LINEAR);
-	imshow("Mask Image", img_mask1);
+	resize(img_mask, img_mask, Size(info.width / 4, info.height / 4), 0, 0, INTER_LINEAR);
+	imshow("Mask Image", img_mask);
 	waitKey(10);
 
 	return false;
